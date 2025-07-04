1ff978f975325e465883d2ed349b0b469565c791
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-04 20:39:21 +0000

    Hero Section Done

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 const Awards = () => {
   return (
-    <div>Awards</div>
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-6'>
+          <img src="media/largestBroker.svg" alt="" />
+        </div>
+        <div className='col-6'>
+          <h1>Largest stock broker in India</h1>
+          <p>2+ million Zerodha client contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+          <div className='row'>
+            <div className='col-6'>
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className='col-6'>
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img src="media/pressLogos.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
