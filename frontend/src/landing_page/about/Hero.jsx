84f09961a3ebb8ee25da3711ca9ca29e85925e04
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-06 01:56:23 +0000

    AboutPage done

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <div className="col-6 p-5">
           <p>
             We kick-started operations on the 15th of August, 2010 with the goal
-            of breaking all barriers that traders and investors face in India in
+            of breaking all barriers that traders and investors face in  India in
             terms of cost, support, and technology. We named the company
             Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
             barrier.</p>
