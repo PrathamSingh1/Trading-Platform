94963e98557223535c4678f6b84f3f580b7433ef
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-06 00:05:47 +0000

    Navbar done

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src="media/homeHero.png" alt="Hero Image" className='mb-5'/>
         <h1 className='mt-5'>Invest in everything</h1>
         <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-        <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin: "0 auto" }}>Signup Now</button>
+        <button className='p-1 btn btn-primary fs-5 mb-5' style={{width:"15%", margin: "0 auto" }}>Signup Now</button>
       </div>
     </div>
   )
