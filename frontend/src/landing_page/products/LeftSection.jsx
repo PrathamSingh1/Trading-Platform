3fb59797aac5fe6f8f8e6fb43f78f2d7a91f5622
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-06 02:54:02 +0000

    ProductsPage done

--- frontend/src/landing_page/products/LeftSection.jsx
+++ frontend/src/landing_page/products/LeftSection.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
-const LeftSection = () => {
+const LeftSection = ({
+  imageURL,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) => {
   return (
-    <div>LeftSection</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <img src={imageURL} alt="" />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={tryDemo}>Try Demo</a>
+            <a href={learnMore} style={{ marginLeft: "50px" }}>
+              Learn More
+            </a>
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img src="media/googlePlayBadge.svg" />
+            </a>
+            <a href={appStore}>
+              <img
+                src="media/appstoreBadge.svg"
+                style={{ marginLeft: "50px" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LeftSection
+export default LeftSection;
